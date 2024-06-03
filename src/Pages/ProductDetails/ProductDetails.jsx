e304e87cce92e87d838987cc4c4d3f75b78b0440
Abdullah Alawa <awamah.group@gmail.com>
2024-06-04 02:29:34 +0300

    finish productDetails Page

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Loading from "../../Components/Loading/Loading";
+import ImageGallery from "react-image-gallery";
 
 export default function ProductDetails() {
   const [details, setDetails] = useState(null);
@@ -9,7 +10,7 @@ export default function ProductDetails() {
 
   async function getProductDetails() {
     let { data } = await axios.get(
-      `https://ecommerce.routemisr.com/api/v1/categories/${id}`
+      `https://ecommerce.routemisr.com/api/v1/products/${id}`
     );
 
     setDetails(data.data);
@@ -20,14 +21,42 @@ export default function ProductDetails() {
     getProductDetails();
   }, []);
 
+  const imageItems = details?.images.map((imageURL) => {
+    return {
+      original: imageURL,
+      thumbnail: imageURL,
+    };
+  });
+
   return (
     <>
       {details === null ? (
         <Loading />
       ) : (
-        <div className="grid grid-cols-12">
-          <div className="cols-span-4"></div>
-          <div className="cols-span-8"></div>
+        <div className="grid grid-cols-12 gap-6">
+          <div className="col-span-3">
+            <ImageGallery
+              items={imageItems}
+              showNav={false}
+              showFullscreenButton={false}
+              showPlayButton={false}
+            />
+          </div>
+          <div className="col-span-9 p-8">
+            <h2 className="text-2xl font-bold">{details.title}</h2>
+            <h3 className="text-primary font-semibold">
+              {details.category.name}
+            </h3>
+            <p className="mt-3">{details.description}</p>
+            <div className="flex justify-between items-center mt-4">
+              <span>{details.price} L.E</span>
+              <span>
+                <i className="fa-solid fa-star text-yellow-400 mr-1"></i>
+                {details.ratingsAverage}
+              </span>
+            </div>
+            <button className="btn-primary w-full mt-4">Add To Cart</button>
+          </div>
         </div>
       )}
     </>
